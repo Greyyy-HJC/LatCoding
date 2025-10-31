044467b7054e38e7b0ffed34cb11989b19a3a1d6
Greyyy-HJC <hejinchen17@gmail.com>
2025-10-31 16:17:46 -0400

    add pyquda mom smearing

--- pyquda_benchmark/meas/pion_disp.py
+++ pyquda_benchmark/meas/pion_disp.py
@@ -1,48 +1,88 @@
 # %%
+import os
+from tqdm.auto import tqdm
 import numpy as np
 import cupy as cp
+from pyquda import init
 from opt_einsum import contract
 from matplotlib import pyplot as plt
-
 from pyquda_utils import core, io, gamma, phase
+from pyquda_utils.phase import MomentumPhase
 
-core.init([1, 1, 1, 1], resource_path=".cache")
-latt_info = core.LatticeInfo([8, 8, 8, 32], -1, 1.0)
+from lametlat.utils.plot_settings import *
+from lametlat.utils.resampling import *
+from lametlat.preprocess.read_raw import pt2_to_meff
 
-dirac = core.getDirac(latt_info, -0.2770, 1e-12, 1000, 1.0, 1.160920226, 1.160920226)
-gauge = io.readNERSCGauge(f"../../conf/S8T32/wilson_b6.0")
-# gauge.stoutSmear(1, 0.125, 4)
-dirac.loadGauge(gauge)
+if not os.path.exists(".cache"):
+    os.makedirs(".cache")
+    print("Created .cache directory for PyQUDA resources")
+
+init([1, 1, 1, 1], resource_path=".cache")
+N_conf = 20
+
+xi_0, nu = 1.0, 1.0
+mass = -0.038888 # kappa = 0.12623
+csw_r = 1.02868
+csw_t = 1.02868
+multigrid = None 
+
+latt_info = core.LatticeInfo([8, 8, 8, 32], -1, xi_0 / nu)
+dirac = core.getClover(latt_info, mass, 1e-8, 10000, xi_0, csw_r, csw_t, multigrid)
 
 G5 = gamma.gamma(15)
 momentum_list = [[0, 0, 0], [0, 0, 1], [0, 1, 1], [1, 1, 1], [0, 0, 2], [0, 1, 2], [1, 1, 2], [0, 2, 2], [1, 2, 2]]
-momentum_phase = phase.MomentumPhase(latt_info).getPhases(momentum_list)
+momentum_label = ["000", "001", "011", "111", "002", "012", "112", "022", "122"]
+momentum_phases = MomentumPhase(latt_info).getPhases(momentum_list)
+
 t_src_list = list(range(0, 32, 32))
-pion = cp.zeros((len(t_src_list), len(momentum_list), latt_info.Lt), "<c16")
 
-for t_idx, t_src in enumerate(t_src_list):
-    propag = core.invert(dirac, "point", [0, 0, 0, t_src])
+data_ls = []
+for cfg in tqdm(range(N_conf), desc="Processing configurations"):
+    gauge = io.readNERSCGauge(f"../../conf/S8T32/wilson_b6.{cfg}")
 
-    pion[t_idx] += contract(
-        "pwtzyx,wtzyxjiba,jk,wtzyxklba,li->pt",
-        momentum_phase,
-        propag.data.conj(),
-        G5 @ G5,
-        propag.data,
-        G5 @ G5,
-    )
+    # gauge.stoutSmear(1, 0.125, 4)
+    dirac.loadGauge(gauge)
+    
+    pion = cp.zeros((len(t_src_list), len(momentum_list), latt_info.Lt), "<c16")
+
+    for t_idx, t_src in enumerate(t_src_list):
+        propag = core.invert(dirac, "point", [0, 0, 0, t_src])
+
+        pion[t_idx] += contract(
+            "pwtzyx,wtzyxjiba,jk,wtzyxklba,li->pt",
+            momentum_phases,
+            propag.data.conj(),
+            G5 @ G5,
+            propag.data,
+            G5 @ G5,
+        )
+        
+    tmp = core.gatherLattice(pion.real.get(), [2, -1, -1, -1])
+    data_ls.append(tmp)
 
 dirac.destroy()
 
-tmp = core.gatherLattice(pion.real.get(), [2, -1, -1, -1])
 if latt_info.mpi_rank == 0:
-    for t_idx, t_src in enumerate(t_src_list):
-        tmp[t_idx] = np.roll(tmp[t_idx], -t_src, 1)
-    twopt = tmp.mean(0)
-    mass = np.arccosh((twopt[:, 2:] + twopt[:, :-2]) / (2 * twopt[:, 1:-1]))
-    for p_idx, mom in enumerate(momentum_list):
-        plt.plot(np.arange(1, 32 - 1), mass[p_idx], ",-", label=f"{mom}")
-    plt.legend()
-    plt.savefig("../plots/pion_dispersion.svg")
-    plt.clf()
+    for idx, tmp in enumerate(data_ls):
+        for t_idx, t_src in enumerate(t_src_list):
+            tmp[t_idx] = np.roll(tmp[t_idx], -t_src, 1)
+        data_ls[idx] = tmp
+        
+    temp = np.array(data_ls)[:,0,:,:]
+    data_avg = jk_ls_avg(jackknife(temp))
+    
+    fig, ax = default_plot()
+    
+    for idx, data_mom in enumerate(data_avg):
+        meff = pt2_to_meff(data_mom, boundary="periodic")
+        ax.errorbar(np.arange(len(meff)), gv.mean(meff), yerr=gv.sdev(meff), label=f"{momentum_label[idx]}", **errorb)
+    ax.legend(ncol=2, **fs_small_p)
+    ax.set_xlabel(r"$t_{\mathrm{sep}}$", **fs_p)
+    ax.set_ylabel(r"$m_{\mathrm{eff}}$", **fs_p)
+    ax.set_ylim(0, 5)
+    ax.legend(ncol=3, loc="upper right", **fs_small_p)
+    plt.tight_layout()
+    plt.savefig("plots/pion_dispersion.pdf", transparent=True)
+    plt.show()
+
 # %%
